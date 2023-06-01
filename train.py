7bab4988b723eb6cb61863b75122b74100021a04
Lixing Tan <lextran@163.com>
2023-06-01 13:28:19 +0800

    Update train

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 import torch.backends.cudnn as cudnn
 import SimpleITK as sitk
 from monai.optimizers import WarmupCosineSchedule
-from monai.networks.nets import UNet
+from monai.networks.nets import UNet, UNETR
 from monai import transforms as tfs
 from monai.inferers import sliding_window_inference
 from monai.losses import DiceLoss, DiceCELoss
@@ -51,13 +51,26 @@ else:
     cudnn.benchmark = False
     device_ids = [1]
 
-model = UNet(
-    spatial_dims=3,
+# model = UNet(
+#     spatial_dims=3,
+#     in_channels=1,
+#     out_channels=int(args.classes),
+#     channels=(16, 32, 64, 128, 256),
+#     strides=(2, 2, 2, 2),
+#     num_res_units=2,
+# )
+model = UNETR(
     in_channels=1,
-    out_channels=int(args.classes),
-    channels=(16, 32, 64, 128, 256),
-    strides=(2, 2, 2, 2),
-    num_res_units=2,
+    out_channels=14,
+    img_size=(96, 96, 96),
+    feature_size=16,
+    hidden_size=768,
+    mlp_dim=3072,
+    num_heads=12,
+    pos_embed="perceptron",
+    norm_name="instance",
+    res_block=True,
+    dropout_rate=0.0,
 )
 
 if args.dp:
